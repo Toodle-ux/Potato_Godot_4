f15a2b732efd79a3e26eb3c927839cf9031b8d39
Zhiyue Huang <zhiyuehuang@yeah.net>
2023-03-05 16:11:40 -0500

    update large potato scripts

--- scripts/tilemap_small.gd
+++ scripts/tilemap_small.gd
@@ -118,15 +118,26 @@ func _combine_potatoes(cell):
 				set_cell(0, current_cell, 0, Vector2i(11, 0))
 		
 		_grow_big_potatoes(cell)
-	
+
+# grow a big potato at the location of four small potatoes	
 func _grow_big_potatoes(cell):
-#	if (cell.x % 2 == 0) && (cell.y % 2 == 0):
-#		var x = cell.x / 2
-#		var y = cell.y / 2
-#		tilemap_large_2.set_cell(0, Vector2i(cell.x / 2, cell.y / 2), 0, Vector2i(0, 0))
+	if (cell.x % 2 == 0) && (cell.y % 2 != 0):
+		var x = cell.x / 2
+		var y = (cell.y + 1) / 2
+		tilemap_large_1.set_cell(0, Vector2i(x, y), 0, Vector2i(0, 0))
+	
+	if (cell.x % 2 == 0) && (cell.y % 2 == 0):
+		var x = cell.x / 2
+		var y = cell.y / 2
+		tilemap_large_2.set_cell(0, Vector2i(x, y), 0, Vector2i(0, 0))
 	
 	if (cell.x % 2 != 0) && (cell.y % 2 != 0):
 		print("big potato")
 		var x = (cell.x - 1) / 2
 		var y = (cell.y + 1) / 2
 		tilemap_large_3.set_cell(0, Vector2i(x, y), 0, Vector2i(0, 0))
+
+	if (cell.x % 2 != 0) && (cell.y % 2 == 0):
+		var x = (cell.x - 1) / 2
+		var y = cell.y / 2
+		tilemap_large_4.set_cell(0, Vector2i(x, y), 0, Vector2i(0, 0))
